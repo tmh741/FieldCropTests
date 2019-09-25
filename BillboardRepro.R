5647e694bc70171d12e28b523becdb719b3a7034
tmh741 <tmh741@bu.edu>
2019-09-25 13:30:40 -0400

    Assignment 2. Added Source

--- BillboardRepro.R
+++ BillboardRepro.R
@@ -5,7 +5,9 @@ library(stringr)
 library(plyr)
 source("xtable.r")
 
-#
+#Like TB, I mostly got this code from Wickham's example.
+#His code can be found at: 
+#https://raw.githubusercontent.com/hadley/tidy-data/master/data/billboard.r
 
 raw.bill <- read.csv("https://raw.githubusercontent.com/hadley/tidy-data/master/data/billboard.csv")
 raw.bill <- raw.bill[,c("year", "artist.inverted", "track", "time", "date.entered", "x1st.week", "x2nd.week", "x3rd.week", "x4th.week", "x5th.week", "x6th.week", "x7th.week", "x8th.week", "x9th.week", "x10th.week", "x11th.week", "x12th.week", "x13th.week", "x14th.week", "x15th.week", "x16th.week", "x17th.week", "x18th.week", "x19th.week", "x20th.week", "x21st.week", "x22nd.week", "x23rd.week", "x24th.week", "x25th.week", "x26th.week", "x27th.week", "x28th.week", "x29th.week", "x30th.week", "x31st.week", "x32nd.week", "x33rd.week", "x34th.week", "x35th.week", "x36th.week", "x37th.week", "x38th.week", "x39th.week", "x40th.week", "x41st.week", "x42nd.week", "x43rd.week", "x44th.week", "x45th.week", "x46th.week", "x47th.week", "x48th.week", "x49th.week", "x50th.week", "x51st.week", "x52nd.week", "x53rd.week", "x54th.week", "x55th.week", "x56th.week", "x57th.week", "x58th.week", "x59th.week", "x60th.week", "x61st.week", "x62nd.week", "x63rd.week", "x64th.week", "x65th.week", "x66th.week", "x67th.week", "x68th.week", "x69th.week", "x70th.week", "x71st.week", "x72nd.week", "x73rd.week", "x74th.week", "x75th.week", "x76th.week")]
